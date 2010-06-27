29eec9d51240d84b1752c8b6df59f4d7a9bc4a90
Pete Nicholls <pete@metanation.com>
2010-06-27 17:25:48 +1200

    Beginning example of how one could power it through Jekyll.

---
+++ _site/public/javascripts/application.js
@@ -0,0 +1,23 @@
+$().ready(function() {
+  $("#twitter-messages").tweet({
+    username: "Aupajo",
+		count: 10,		
+    join_text: "<a href='http://twitter.com/Aupajo'>Pete</a> wrote",
+    avatar_size: 40,
+    loading_text: "Loading tweets&hellip;"
+	});
+	
+	$('#nav a').click(function() {
+	  showSection($(this).attr('href'));
+	  return false;
+	});
+	
+	showSection($('#nav li:first-child a').attr('href'));
+});
+
+function showSection(section) {
+  $('#article .section').hide();
+  $('#nav a').removeClass('selected');
+  $('#nav a[href=' + section + ']').addClass('selected');
+  $(section).show();
+}
---
+++ _site/sass/config.rb
@@ -0,0 +1,9 @@
+# Require any additional compass plugins here.
+project_type = :stand_alone
+ 
+sass_dir = "."
+css_dir = "../public/stylesheets"
+output_style = :compressed
+ 
+# Enable relative paths to assets
+relative_assets = true
